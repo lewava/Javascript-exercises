cd3bcfa23117c0d1cd122fc3394bc3b05a9dc028
lewava <leon.wass.vallin@gmail.com>
2020-10-21 20:40:11 +0200

    commit

---
+++ dom-manipulering.js
@@ -0,0 +1,33 @@
+/*---------- UPPGIFT 1 ----------*/
+
+let input = document.querySelector('input');
+let btn = document.querySelector('button');
+let ptag = document.querySelector('p');
+
+function changeText(e) {
+
+    ptag.textContent = input.value;
+    input.value = '';
+
+}
+
+btn.addEventListener('click', changeText);
+
+/*---------- UPPGIFT 2 ----------*/
+
+let calcOutput = document.querySelector('.calc-output');
+let calcInput = document.querySelector('.calc-input')
+let buttonMinus = document.querySelector('calc-minus')
+let buttonPlus = document.querySelector('.calc-plus')
+let buttonEquals = document.querySelector('.calc-equals')
+let calculation = '';
+
+buttonMinus.addEventListener('click', )
+
+function calculate(input) {
+    let tempArray = input.split('');
+
+    for (let element of tempArray) {
+        if
+    }
+}
