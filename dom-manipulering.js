0217a02ec99d55897229b6f89b355d4db838c525
Leon Wass Vallin <leon.wass.vallin@mail.com>
2020-10-23 13:21:34 +0200

    commit

--- dom-manipulering.js
+++ dom-manipulering.js
@@ -17,14 +17,58 @@ btn.addEventListener('click', changeText);
 
 let calcOutput = document.querySelector('.calc-output');
 let calcInput = document.querySelector('.calc-input')
-let buttonMinus = document.querySelector('calc-minus')
+let buttonMinus = document.querySelector('.calc-minus')
 let buttonPlus = document.querySelector('.calc-plus')
 let buttonEquals = document.querySelector('.calc-equals')
 let calculation = '';
 
-buttonMinus.addEventListener('click', )
+buttonMinus.addEventListener('click', function() {
+    calculation += calcInput.value + ' - ';
+    calcOutput.textContent = calculation;
+    calcInput.value = '';
+});
+buttonPlus.addEventListener('click', function() {
+    calculation += calcInput.value + ' + ';
+    calcOutput.textContent = calculation;
+    calcInput.value = '';
+});
+buttonEquals.addEventListener('click', function() {
+    calculation += calcInput.value;
+    let temp = eval(calculation);
+    calcOutput.textContent = calculation + ' = ' + temp;
+    calcInput.value = '';
+});
 
-function calculate(input) {
-    let tempArray = input.split('');
+/*---------- UPPGIFT 3 ----------*/
 
-}
+let articles = [{
+        name: "Nike skor",
+        price: 1000,
+        description: "Lorem ipsum dolor sit amet consectetur elit.Accusamus,iste suscipit praesentium aliquid natus unde veniam.Omnis ullam vitae animi voluptas.Asperiores aperiam veniam sapiente necessitatibus et totam velit dolor."
+    },
+    {
+        name: "Filippa K byxa",
+        price: 10000,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Accusamus, iste suscipit praesentium aliquid natus unde veniam.Omnis ullam vitae animi voluptas.Asperiores aperiam veniam sapiente necessitatibus et totam velit dolor."
+    },
+    {
+        name: "Adidas tröja",
+        price: 2000,
+        description: "Lorem ipsum dolor sit amet consectetur elit.Accusamus,iste suscipit praesentium aliquid natus unde veniam.Omnis ullam vitae animi voluptas.Asperiores aperiam veniam sapiente necessitatibus et totam velit dolor."
+    },
+    {
+        name: "Levis jeans",
+        price: 1000,
+        description: "Lorem ipsum dolor sit amet consectetur elit.Accusamus,iste suscipit praesentium aliquid natus unde veniam.Omnis ullam vitae animi voluptas.Asperiores aperiam veniam sapiente necessitatibus et totam velit dolor."
+    },
+    {
+        name: "Lee jeans",
+        price: 1100,
+        description: "Lorem ipsum dolor sit amet consectetur elit.Accusamus,iste suscipit praesentium aliquid natus unde veniam.Omnis ullam vitae animi voluptas.Asperiores aperiam veniam sapiente necessitatibus et totam velit dolor."
+    },
+    {
+        name: "Convers skor",
+        price: 600,
+        description: "Lorem ipsum dolor sit amet consectetur elit.Accusamus,iste suscipit praesentium aliquid natus unde veniam.Omnis ullam vitae animi voluptas.Asperiores aperiam veniam sapiente necessitatibus et totam velit dolor."
+    }
+];
